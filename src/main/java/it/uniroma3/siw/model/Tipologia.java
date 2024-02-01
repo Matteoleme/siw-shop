6812785ba4e3f3ec9ccd205bc6263f4ac396e785
Matteoleme <matteo.mastranza01@gmail.com>
2024-02-01 16:47:32 +0100

    Add tipologia prodotto

--- src/main/java/it/uniroma3/siw/model/Tipologia.java
+++ src/main/java/it/uniroma3/siw/model/Tipologia.java
@@ -1,5 +1,68 @@
 package it.uniroma3.siw.model;
 
-public class Tipologia {
+import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Tipologia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	private String nome;
+	
+	@OneToMany(mappedBy = "Tipologia")
+	private List<Prodotto> prodotti;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Prodotto> getProdotti() {
+		return prodotti;
+	}
+
+	public void setProdotti(List<Prodotto> prodotti) {
+		this.prodotti = prodotti;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tipologia other = (Tipologia) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
+	
 }
