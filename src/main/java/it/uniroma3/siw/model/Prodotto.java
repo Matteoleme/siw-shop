4d202db39345b5a5666e2fc4e4ddcefa7541e147
Matteoleme <matteo.mastranza01@gmail.com>
2024-02-01 13:33:23 +0100

    Update Prodotto.java

--- src/main/java/it/uniroma3/siw/model/Prodotto.java
+++ src/main/java/it/uniroma3/siw/model/Prodotto.java
@@ -1,11 +1,14 @@
 package it.uniroma3.siw.model;
 
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 public class Prodotto {
@@ -18,10 +21,80 @@ public class Prodotto {
 	
 	private String descrizione;
 	
+	@Positive
 	private Float prezzo;
 
 	@ManyToOne
 	private Tipologia tipologia;
+	
+	@ManyToOne
+	private Marca marca;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Float prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public Tipologia getTipologia() {
+		return tipologia;
+	}
+
+	public void setTipologia(Tipologia tipologia) {
+		this.tipologia = tipologia;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prodotto other = (Prodotto) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
 	
 	
 }
