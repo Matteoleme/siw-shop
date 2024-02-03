9622bb27ce586b5cededaea64d1995a1ca5248c2
Matteoleme <matteo.mastranza01@gmail.com>
2024-02-03 17:56:39 +0100

    Update CredenzialiService.java
    Metodo di supporto per credenziali passato un oggetto UserDetails

--- src/main/java/it/uniroma3/siw/service/CredenzialiService.java
+++ src/main/java/it/uniroma3/siw/service/CredenzialiService.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +16,10 @@ public class CredenzialiService {
 
 	@Autowired
 	protected PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	protected CredenzialiRepository credenzialiRepository;
-	
+
 	@Transactional
 	public Credenziali getCredenziali(Long id) {
 		Optional<Credenziali> result = this.credenzialiRepository.findById(id);
@@ -30,11 +31,21 @@ public class CredenzialiService {
 		Optional<Credenziali> result = this.credenzialiRepository.findByUsername(username);
 		return result.orElse(null);
 	}
-		
-    @Transactional
-    public Credenziali saveCredentials(Credenziali credenziali) {
-        credenziali.setRuolo(Credenziali.DEFAULT_ROLE);
-        credenziali.setPassword(this.passwordEncoder.encode(credenziali.getPassword()));
-        return this.credenzialiRepository.save(credenziali);
-    }
+
+	@Transactional
+	public Credenziali getCredenziali(UserDetails user) {
+		if(user != null) {
+			return this.credenzialiRepository.findByUsername(user.getUsername()).orElse(null);
+		}
+		else {
+			return null;
+		}
+	}
+
+	@Transactional
+	public Credenziali saveCredentials(Credenziali credenziali) {
+		credenziali.setRuolo(Credenziali.DEFAULT_ROLE);
+		credenziali.setPassword(this.passwordEncoder.encode(credenziali.getPassword()));
+		return this.credenzialiRepository.save(credenziali);
+	}
 }
