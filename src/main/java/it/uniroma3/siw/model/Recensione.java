c3ea695f2162aee21b277a971ff6f16c6f014bef
Matteoleme <matteo.mastranza01@gmail.com>
2024-02-04 12:46:51 +0100

    Create Recensione.java

---
+++ src/main/java/it/uniroma3/siw/model/Recensione.java
@@ -0,0 +1,101 @@
+package it.uniroma3.siw.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Recensione {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotBlank
+	private String descrizione;
+	@Min(value = 0)
+	@Max(value = 5)
+	private Integer valutazione;
+
+	private LocalDate timestamp;
+
+	@ManyToOne
+	private Utente utente;
+
+	@ManyToOne
+	private Prodotto prodotto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public Integer getValutazione() {
+		return valutazione;
+	}
+
+	public void setValutazione(Integer valutazione) {
+		this.valutazione = valutazione;
+	}
+
+	public LocalDate getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDate timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+
+	public Prodotto getProdotto() {
+		return prodotto;
+	}
+
+	public void setProdotto(Prodotto prodotto) {
+		this.prodotto = prodotto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(prodotto, utente);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recensione other = (Recensione) obj;
+		return Objects.equals(prodotto, other.prodotto) && Objects.equals(utente, other.utente);
+	}
+
+}
